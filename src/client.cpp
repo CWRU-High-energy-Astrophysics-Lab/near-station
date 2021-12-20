8c11dfc70a4ff08d19eb0e1065eebdb4f828278a
robin <rnj13@gmail.com>
2021-12-20 18:18:39 -0500

    network

--- src/client.cpp
+++ src/client.cpp
@@ -1,52 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
+//
+// blocking_tcp_echo_client.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
-}
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <boost/asio.hpp>
 
-int client(const char* host, int portno, char buffer[])
-{
-    int sockfd, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+using boost::asio::ip::tcp;
+
+enum { max_length = 1024 };
+
+int send(std::string hostname, std::string port, std::string msg) {
+    try {
+
+
+        boost::asio::io_service io_service;
+
+        tcp::resolver resolver(io_service);
+        tcp::resolver::query query(tcp::v4(), hostname, port);
+        tcp::resolver::iterator iterator = resolver.resolve(query);
+
+        tcp::socket s(io_service);
+        boost::asio::connect(s, iterator);
+
+        using namespace std; // For strlen.
 
 
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        return -2; //error("ERROR opening socket");
-    server = gethostbyname(host);
-    if (server == NULL) {
-        //fprintf(stderr,"ERROR, no such host\n");
-        return  -3;// exit(0);
+
+        size_t request_length = strlen(msg.c_str());
+        boost::asio::write(s, boost::asio::buffer(msg.c_str(), request_length));
+
+        char reply[max_length];
+        size_t reply_length = boost::asio::read(s,
+                                                boost::asio::buffer(reply, request_length));
+        std::cout << "Reply is: ";
+        std::cout.write(reply, reply_length);
+        std::cout << "\n";
+
+
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-          (char *)&serv_addr.sin_addr.s_addr,
-          server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-       return -4;// error("ERROR connecting");
+    catch (std::exception &e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
 
-
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0)
-        error("ERROR writing to socket");
-    bzero(buffer,sizeof(buffer));
-    n = read(sockfd,buffer,sizeof(buffer));
-    if (n < 0)
-        error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
     return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,104 +1,132 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 
+#include <cstdlib>
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
 
-#include <unistd.h>
+using boost::asio::ip::tcp;
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-
-#include "centralinterface.h"
-//#include "NearProccess.h"
-
-using namespace std;
-void error(const char *msg)
+class session
 {
-    perror(msg);
-    exit(0);
-}
-
-int server(int portno)
-{
-    int sockfd, newsockfd;
-    socklen_t clilen;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        return -1;//error("ERROR opening socket");
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-             sizeof(serv_addr)) < 0)
-        return -2; //error("ERROR on binding");
-    listen(sockfd,5);
-    clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd,
-                       (struct sockaddr *) &cli_addr,
-                       &clilen);
-    if (newsockfd < 0)
-        return -3;//error("ERROR on accept");
-    bzero(buffer,256);
-    n = read(newsockfd,buffer,255);
-    if (n < 0) return -4;//error("ERROR reading from socket");
-    addmsgtoCentral(decrypt(buffer));
-    n = write(newsockfd,"I got your message",18);
-    if (n < 0) return -5; //error("ERROR writing to socket");
-    //close(newsockfd);
-    //close(sockfd);
-    return 0;
-}
-
-
-
-int client(const char* host, int portno, char buffer[])
-{
-    int sockfd, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        return -2; //error("ERROR opening socket");
-    server = gethostbyname(host);
-    if (server == NULL) {
-        //fprintf(stderr,"ERROR, no such host\n");
-        return  -3;// exit(0);
+public:
+    session(boost::asio::io_service& io_service)
+            : socket_(io_service)
+    {
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-          (char *)&serv_addr.sin_addr.s_addr,
-          server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-        return -4;// error("ERROR connecting");
 
+    tcp::socket& socket()
+    {
+        return socket_;
+    }
 
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0)
-        error("ERROR writing to socket");
-    bzero(buffer,sizeof(buffer));
-    n = read(sockfd,buffer,sizeof(buffer));
-    if (n < 0)
-        error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
+    void start()
+    {
+        socket_.async_read_some(boost::asio::buffer(data_, max_length),
+                                boost::bind(&session::handle_read, this,
+                                            boost::asio::placeholders::error,
+                                            boost::asio::placeholders::bytes_transferred));
+    }
+
+private:
+    void handle_read(const boost::system::error_code& error,
+                     size_t bytes_transferred)
+    {
+        if (!error)
+        {
+            boost::asio::async_write(socket_,
+                                     boost::asio::buffer(data_, bytes_transferred),
+                                     boost::bind(&session::handle_write, this,
+                                                 boost::asio::placeholders::error));
+
+            std::cerr << data_;
+
+        }
+        else
+        {
+            delete this;
+        }
+    }
+
+    void handle_write(const boost::system::error_code& error)
+    {
+        if (!error)
+        {
+            socket_.async_read_some(boost::asio::buffer(data_, max_length),
+                                    boost::bind(&session::handle_read, this,
+                                                boost::asio::placeholders::error,
+                                                boost::asio::placeholders::bytes_transferred));
+        }
+        else
+        {
+            delete this;
+        }
+    }
+
+    tcp::socket socket_;
+    enum { max_length = 1024 };
+    char data_[max_length];
+};
+
+class server
+{
+public:
+    server(boost::asio::io_service& io_service, short port)
+            : io_service_(io_service),
+              acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+    {
+        start_accept();
+    }
+
+private:
+    void start_accept()
+    {
+        session* new_session = new session(io_service_);
+        acceptor_.async_accept(new_session->socket(),
+                               boost::bind(&server::handle_accept, this, new_session,
+                                           boost::asio::placeholders::error));
+    }
+
+    void handle_accept(session* new_session,
+                       const boost::system::error_code& error)
+    {
+        if (!error)
+        {
+            new_session->start();
+        }
+        else
+        {
+            delete new_session;
+        }
+
+        start_accept();
+    }
+
+    boost::asio::io_service& io_service_;
+    tcp::acceptor acceptor_;
+};
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: async_tcp_echo_server <port>\n";
+            return 1;
+        }
+
+        boost::asio::io_service io_service;
+
+        using namespace std; // For atoi.
+        server s(io_service, atoi(argv[1]));
+
+        io_service.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
     return 0;
 }
+
