549abbdcd665240ebd90b43b233684a0dd520752
robin <rnj13@gmail.com>
2022-05-06 10:27:40 -0400

    segment fault test

--- src/NearProccess.cpp
+++ src/NearProccess.cpp
@@ -207,7 +207,7 @@ string getmsgToSend() {
 
 //Functions to msgToUnpack
 void addmsgtoUnpack(string incoming) {
-    std::cout<< incoming;
+    //std::cout<< incoming;
     mu.lock();
 
     msgToUnPack.push(incoming);
