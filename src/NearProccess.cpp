b10a1b58a31fa86a0c4e4c6e2734cbcd787b4b5f
robin <rnj13@gmail.com>
2022-05-09 13:09:11 -0400

    segment fault test

--- src/NearProccess.cpp
+++ src/NearProccess.cpp
@@ -88,10 +88,10 @@ int NearProccess::start() {
         std::cout<< msgToUnPack.size();
         auto msg = getmsgToUnpack(); // this is always returning an empty string
 
-        if(msg.length()>0){
-            std::cout<< "add\n";
-            addmsgtoProccess(msg);
-        }
+
+
+        addmsgtoProccess(msg);
+        /*
         if (!msgtoProccessEmpty()) {
             Generalmsg msg = getmsgToProccess();
             string type = msg.gedID();
@@ -127,7 +127,7 @@ int NearProccess::start() {
                 std::cout << encrypt(msg);
                 //add a report to log
             }
-        }
+        }*/
         while(ismsgFromNetIn()!=true){
 
             addmsgtoProccess(encrypt(getmsgToNetIn()));
